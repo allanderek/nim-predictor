6da5288d0cc916e16bdcbc23d8e4dd5dce9a593d
allanderek <allan.clark@gmail.com>
2024-01-10 09:37:03 +0000

    Change the head template so that it doesn't say 'Prologue' all the time.

--- src/templates/share/head.nim
+++ src/templates/share/head.nim
@@ -4,12 +4,8 @@ import karax/[karaxdsl, vdom]
 
 # This is <head> html section. It's shared across all templates
 proc sharedHead*(ctx: Context, title: string): VNode =
-  let
-    env = loadPrologueEnv(".env")
-    appName = env.getOrDefault("appName", "Prologue")
-
   let vNode = buildHtml(head):
-    title: text title & " - " & appName
+    title: text title 
     # link(rel = "stylesheet", href = "/static/mvp.css")
     link(rel = "stylesheet", href = "https://cdn.jsdelivr.net/gh/kimeiga/bahunya/dist/bahunya.min.css")
   return vNode
